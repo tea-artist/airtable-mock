9cf16c3b541273e2a50fad068c1f7cf3826588b6
tea artist <artist@teable.io>
2024-04-06 10:38:02 +0000

    feat: add basic ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,34 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
+      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        Mock data for Airtable
+      </h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Access token</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <Input placeholder="API access token for airtable" />
+          </form>
+        </CardContent>
+        <CardHeader>
+          <CardTitle>Table name</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <Input placeholder="Which table do you want to import data into" />
+          </form>
+        </CardContent>
+        <CardFooter className="border-t px-6 py-4">
+          <Button>Save</Button>
+        </CardFooter>
+      </Card>
     </section>
   )
 }
